1ead305b6176ead7d0cc307e398097d62be544b6
LilyVL <s098448@ap.be>
2018-09-03 16:19:31 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port 3000');
+});
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+   res.redirect('/Question')
+})
+
+app.get('/Question', (req, res) => {
+   res.render('Question.ejs', {})
+})
