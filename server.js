a6c485358d4ae89a5bbf368c200ada767a25a536
LilyVL <s098448@ap.be>
2018-09-03 16:32:12 +0200

    Can now answer questions

--- server.js
+++ server.js
@@ -12,9 +12,27 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-   res.redirect('/Question')
-})
+   res.redirect('/Question');
+});
 
 app.get('/Question', (req, res) => {
-   res.render('Question.ejs', {})
-})
+   res.render('Question.ejs', {});
+});
+
+app.post('/Answer', (req, res) => {
+ var question = req.body.question;
+ console.log(question);
+ var answerList = ["Yes.", "No.", "Maybe...", "Absolutely Not!", "Definitely!", "Ask Again Later."];
+ var newAnswer = Math.floor(Math.random() * 6);
+
+ /*if(window.localStorage.getItem(question) == null) {
+ 	var newAnswer = Math.floor(Math.random() * 6);
+ 	window.localStorage.setItem(question, answerList[newAnswer]);
+ }*/
+ res.render('Answer.ejs', { answer: [question, answerList[newAnswer]]});
+});
+
+app.get('/Answer', (req, res) => {
+   res.render('Answer.ejs', {});
+
+});
